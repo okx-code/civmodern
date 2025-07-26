5880b5e0b48a474c4b773a034dde4a0f32ac2861
okx-code <okx@okx.sh>
2025-07-27 00:03:46 +0100

    fix loading world twice

--- common/src/main/java/sh/okx/civmodern/common/map/WorldListener.java
+++ common/src/main/java/sh/okx/civmodern/common/map/WorldListener.java
@@ -150,6 +150,7 @@ public class WorldListener {
                     }, "Map converter");
                     converter.start();
                 }));
+                return;
             }
         }
 
