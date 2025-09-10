4566bdaf601fdaa7e6828bc68cd1e84438be0d81
okx-code <okx@okx.sh>
2025-09-10 02:45:48 +0100

    fix minimap coords

--- src/main/java/sh/okx/civmodern/common/map/Minimap.java
+++ src/main/java/sh/okx/civmodern/common/map/Minimap.java
@@ -150,7 +150,7 @@ public class Minimap {
             ((source, stack) -> renderers.forEach(r -> r.render(source, stack)))));
 
         if (config.isShowMinimapCoords()) {
-            event.guiGraphics().drawString(font, "(%d, %s, %d)".formatted(playerBX, playerBY, playerBZ), 0, 0, 0xFFFFFF);
+            event.guiGraphics().drawCenteredString(mc.font,  "(%d, %s, %d)".formatted(playerBX, playerBY, playerBZ), (int) (size / 2), (int) size + 2, -1);
         }
         if (config.isPlayerWaypointsEnabled()) {
             // TODO fix the player rendering above the chevron
